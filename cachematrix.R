42a5ac7dad4367d0cb9007fde537a1e3294e53b8
jdg2 <jdgundrum2@gmail.com>
2016-07-24 20:34:06 -0500

    functions completed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## these functions allow a user to save time on getting the inverse
+## of a matrix by using a cache to return the inverse... 
+## if the matrix has already been solved
 
-## Write a short comment describing this function
+## this first function takes a matrix input
+## and outputs a list of functions to be used in cacheSolve
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  mtrx_inv <<- NULL
+  set <- function(y) {
+    x<<- y
+    mtrx_inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) mtrx_inv <<- solve 
+  getinv <- function() mtrx_inv
+  list(set = set, get = get, setinv = setinv, getinv = getinv) ##name functions
 }
-
-
-## Write a short comment describing this function
+## This function tests so see if the cach is null
+## if the cache is not null it returns the cache
+## if the cache is not null it returns the cached value
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  mtrx_inv <- x$getinv()       ##calls the inverse from makeCacheMatrix
+  if(!is.null(mtrx_inv)) {    ## checks to see if it is not null
+    message("hold on, gettin the inverse")
+    return(mtrx_inv)     ## returns the matrix if it not null
+  }
+  data<-x$get()              ## returns that matrix
+  mtrx_inv<- solve(data, ...) ## solve the matrix if it is null
+  x$setinv(mtrx_inv)     ## cache the inverse 
+  mtrx_inv               ## call the inverse for displat
 }
